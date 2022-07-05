280d10e40a785734143fb8159b639c85da253df4
Loren <lorensprague@icloud.com>
2022-07-05 01:47:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(friend, birthday) {
+    let thankYouCards = []
+    for ( let i = 0; i < friend.length; i++ ) {
+      thankYouCards.push( `Thank you, ${friend[i]}, for the wonderful ${birthday} gift!` )
+    }
+    return thankYouCards;
+}
+
+function countDown(positiveInteger) {
+    while (positiveInteger > 0) {
+        console.log(positiveInteger);
+        positiveInteger -= 1;
+    }
+    console.log(positiveInteger);
+}
